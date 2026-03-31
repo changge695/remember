f84b3397fe5808609fbacac983e9816979179ac9
changge695 <miseav6@gmail.com>
2026-04-01 01:19:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo 4 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+
 REMOTE_URL="https://raw.githubusercontent.com/changge695/remember/refs/heads/main/sync"
 LOCAL_FILE="async"
 LOG_FILE="async.log"
