873198d2be55f3c90148db20b42ff00f4d389c0f
changge695 <miseav6@gmail.com>
2026-04-01 00:36:40 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REMOTE_URL="https://raw.githubusercontent.com/changge695/remember/refs/heads/main/async"
+LOCAL_FILE="async"
+LOG_FILE="async.log"
+
+if [ -f "$LOCAL_FILE" ]; then
+    pids=$(pgrep -f "$LOCAL_FILE")
+    if [ -n "$pids" ]; then
+        kill -9 $pids 2>/dev/null
+    fi
+else
+    curl -L "$REMOTE_URL" -o "$LOCAL_FILE"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+
+chmod +x "$LOCAL_FILE"
+nohup ./"$LOCAL_FILE" > "$LOG_FILE" 2>&1 &
